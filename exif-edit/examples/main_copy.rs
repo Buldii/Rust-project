713bf809e9f0672a4d2b1a018fee837fec155ee1
Jacob898 <jstach898@gmail.com>
2025-06-02 15:35:30 +0200

    Add extracting and deleting JPF exifs

--- exif-edit/examples/main_copy.rs
+++ exif-edit/examples/main_copy.rs
@@ -1,12 +1,14 @@
-use img_parts::jpeg::{Jpeg, markers};
-use std::fs::File;
-use std::io::{Read, Cursor};
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Cursor, Write};
 use std::path::Path;
 use exif::Reader;
+use img_parts::{jpeg::{Jpeg, markers}};
+use img_parts::ImageEXIF;
 
 fn main() {
-    let path = Path::new("C:/Users/Kuba/Downloads/Canon_PowerShot_S40.png"); 
-
+    let path = Path::new("C:/Users/Kuba/Downloads/Kodak_CX7530.jpg");
+    // remove_exif("C:/Users/Kuba/Downloads/Kodak_CX7530.jpg".as_ref(), "C:/Users/Kuba/Downloads/Kodak_CX7530.jpg".as_ref()).expect("failed");
     match extract_exif_tags(path) {
         Ok(tags) => {
             println!("EXIF tagi (tag, wartość):");
@@ -23,33 +25,53 @@ fn extract_exif_tags(path: &Path) -> Result<Vec<(String, String)>, Box<dyn std::
     let mut data = Vec::new();
     file.read_to_end(&mut data)?;
 
+
     let jpeg = Jpeg::from_bytes(data.into())?;
 
     for segment in jpeg.segments() {
         if segment.marker() == markers::APP1 {
             let content = segment.contents();
-            if content.starts_with(b"Exif\0\0") {
-                // Pomijamy nagłówek b"Exif\0\0", który ma długość 6 bajtów
-                let exif_data = &content[6..];
-                let mut cursor = Cursor::new(exif_data);
-                let reader = Reader::new();
-                let exif = reader.read_from_container(&mut cursor)?;
 
-                
-                let mut tags = Vec::new();
-                for field in exif.fields() {
-                    tags.push((
-                        field.tag.to_string(),
-                        field.display_value().with_unit(&exif).to_string(),
-                    ));
-                }
-                return Ok(tags);
+            if content.starts_with(b"Exif\0\0") {
+                let exif_data = &content[6..];
+                return parse_exif(exif_data);
+            } else {
+                return parse_exif(content);
             }
+
         }
+
     }
 
     Err("Brak danych EXIF w pliku".into())
 }
 
+fn parse_exif(exif_data: &[u8]) -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
+    let mut cursor = Cursor::new(exif_data);
+    let reader = Reader::new();
+    let exif = reader.read_from_container(&mut cursor)?;
+
+    let mut tags = Vec::new();
+
+    for field in exif.fields() {
+        tags.push((
+            field.tag.to_string(),
+            field.display_value().with_unit(&exif).to_string(),
+        ));
+    }
+    Ok(tags)
+}
+
+fn remove_exif(input: &Path, output: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let input_data = fs::read(input)?;
+    let mut jpeg = Jpeg::from_bytes(input_data.into())?;
+    
+    jpeg.set_exif(None);
+    
+    let mut out_file = File::create(output)?;
+    jpeg.encoder().write_to(&mut out_file)?;
+
+    Ok(())
+}
 
 
